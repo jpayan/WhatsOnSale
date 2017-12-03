1c3a14db04578d35c8a31a76e36126eb7f1dcb8c
fidelfcastro <fidel.castro@cetys.edu.mx>
2017-12-02 22:19:58 -0800

    Add Sale Details

---
+++ app/src/main/java/mx/cetys/jorgepayan/whatsonsale/Controllers/SaleDetailsActivity.java
@@ -0,0 +1,13 @@
+package mx.cetys.jorgepayan.whatsonsale.Controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SaleDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_sale_details );
+    }
+}
