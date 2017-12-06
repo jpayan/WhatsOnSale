15a7afc52bb92c84477e42ac8ce542626141d9ce
fidelfcastro <fidel.castro@cetys.edu.mx>
2017-12-05 16:33:34 -0800

    Add Customer Views 1

---
+++ app/src/main/java/mx/cetys/jorgepayan/whatsonsale/Controllers/Activities/ReviewDetailsActivity.java
@@ -0,0 +1,13 @@
+package mx.cetys.jorgepayan.whatsonsale.Controllers.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReviewDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_review_details );
+    }
+}
---
+++ app/src/main/java/mx/cetys/jorgepayan/whatsonsale/Controllers/Adapters/SaleLocationAdapter.java
@@ -0,0 +1,8 @@
+package mx.cetys.jorgepayan.whatsonsale.Controllers.Adapters;
+
+/**
+ * Created by fidel on 12/5/2017.
+ */
+
+public class SaleLocationAdapter {
+}
